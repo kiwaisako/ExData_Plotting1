8880acbd590479a3fbee03f95e87ae24fd60cde0
kiwaisako <keiko.iwaisako@gmail.com>
2016-02-28 12:01:51 -0800

    Added files via upload

---
+++ plot1.R
@@ -0,0 +1,9 @@
+#Read and subset data
+dataFile <- "./data/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+dataSubset <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#Plot 1
+globalActivePower <- as.numeric(dataSubset$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
---
+++ plot2.R
@@ -0,0 +1,11 @@
+#Read and subset data
+dataFile <- "./data/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+dataSubset <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#Plot 2
+datetime <- strptime(paste(dataSubset$Date, dataSubset$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(dataSubset$Global_active_power)
+png("plot2.png", width=480, height=480)
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
